73c117d5de72c29741158842caebbf6884b75b44
NghiaBui <nghiabv120100@gmail.com>
2021-04-02 05:03:18 +0700

    update save findById findByProperty

--- src/main/java/com/website/dao/GenericDAO.java
+++ src/main/java/com/website/dao/GenericDAO.java
@@ -1,10 +1,7 @@
 package com.website.dao;
 
 import com.website.utils.HibernateUtil;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 //import sun.net.www.content.text.Generic;
 
 import java.io.Serializable;
@@ -24,26 +21,108 @@ public class GenericDAO<ID extends Serializable,T> {
 
     }
 
-    protected Session getSession() {
-        return HibernateUtil.getSessionFactory().openSession();
-    }
-
     public List<T> findAll() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
         List<T> list = new ArrayList<T>();
         Transaction transaction = null;
         try {
-            transaction = getSession().beginTransaction();
+            transaction = session.beginTransaction();
             StringBuilder sql = new StringBuilder("from ");
             sql.append(this.getPersistenceClassName());
-            Query query = this.getSession().createQuery(sql.toString());
+            Query query = session.createQuery(sql.toString());
             list = query.list();
             transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
             throw e;
         } finally {
-
+            session.close();
         }
         return list;
+    }
+
+    public T update(T entity) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        T result = null;
+        Transaction transaction = session.beginTransaction();
+        try {
+            Object obj = session.merge(entity);
+            result = (T) obj;
+            transaction.commit();
+        }
+        catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+        return  result;
+    }
+
+    public void save(T entity) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(entity);
+            transaction.commit();
+        }
+        catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    public T findById(ID id) {
+        T result = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            result = (T) session.get(persistenceClass,id);
+            if (result==null) {
+                throw new ObjectNotFoundException("Not found "+id,null);
+            }
+        }
+        catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    public Object[] findByProperty(String property,Object value,String sortExpression, String sortDirection ) {
+        List<T> list = new ArrayList<T>();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+
+            StringBuilder sql = new StringBuilder("from ");
+            sql.append(getPersistenceClassName());
+            sql.append(" where 1=1 ");
+            if (property != null && value != null) {
+                sql.append("and "+property).append(" =:value");
+            }
+            if (sortExpression != null && sortDirection != null) {
+                sql.append(" order by "+sortExpression).append(" "+ sortDirection);
+            }
+
+            Query query1 = session.createQuery(sql.toString());
+            if (property != null && value != null) {
+                query1.setParameter("value",value);
+            }
+
+            list = query1.list();
+
+        }
+        catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+        return new Object[] {list.size(),list};
     }
 }
--- src/test/java/com/website/test/UserTest.java
+++ src/test/java/com/website/test/UserTest.java
@@ -12,4 +12,45 @@ public class UserTest {
         UserDAO userDAO = new UserDAO();
         List<UserEntity> list = userDAO.findAll();
     }
+
+    @Test
+    public void checkUpdate() {
+        UserDAO userDAO = new UserDAO();
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+        userEntity.setFullName("Nghia");
+        UserEntity  newEntity=  userDAO.update(userEntity);
+
+    }
+
+    @Test
+    public void checkSave() {
+        UserDAO userDAO = new UserDAO();
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(2);
+        userEntity.setFullName("NghiaBui");
+        userDAO.save(userEntity);
+
+    }
+
+    @Test
+    public void checkFindById() {
+        UserDAO userDAO = new UserDAO();
+
+        UserEntity u = userDAO.findById(1);
+
+    }
+
+    @Test
+    public void checkFindByByProperty() {
+        UserDAO userDAO = new UserDAO();
+        String property ="fullName";
+        Object value = "NghiaBui12";
+        String sortExpression ="id";
+        String sortDirection ="DESC";  //ASC|DESC
+        Object[] objects = userDAO.findByProperty(property,value,sortExpression,sortDirection);
+
+    }
 }
